af1d5b3e3ed8f8cdadd4f4717110902b09e0e324
Hasha <d.trandafir@student.fontys.nl>
2026-08-04 09:09:51 +0300

    minor commit

---
+++ src/main/java/net/hasha/kafkamod/KafkaModClient.java
@@ -0,0 +1,12 @@
+package net.hasha.kafkamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KafkaModClient implements ClientModInitializer
+{
+    @Override
+    public  void onInitializeClient()
+    {
+
+    }
+}
